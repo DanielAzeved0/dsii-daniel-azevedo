d38247532667896f05d6e9006465ec2a65c12303
unknown <daniel.azevedo081205@gmail.com>
2025-08-05 20:39:04 -0300

    Primeira aula de Python

---
+++ Arvore.py
@@ -0,0 +1,24 @@
+print('       * ')
+print('      *** ')
+print('     ***** ')
+print('    ******* ')
+print('   ********* ')
+print('  *********** ')
+print(' ************* ')
+print('      ***\n      ***\n      ***\n')
+# com \n eu posso colocar varios prints em uma linha só 
+
+x = 3
+print ('x')
+#isso é uma declaração de variavel, junto com a sua adiçãpo de valor e um print exibindo o seu valor
+
+x = 3
+print (type('x'))
+#com o comando type eu consigo saber o tipo do valor da varival
+
+x = input('Digite um numero: ')
+print(x)
+
+x = int(input('Digite um numero: '))
+print(x)
+#se determina o tipo do valor da variavel antes do input(botão)
